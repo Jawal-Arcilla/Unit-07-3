c500cbe9af85ed2c705834957e4aa94c9ce08f68
JawalArcilla <>
2021-05-06 19:11:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('button').addEventListener('click', age)
+
+let userInput = 0
+
+function age () {
+
+  userInput = document.getElementById('input').value
+  userInput = parseInt(userInput)
+
+  if (userInput >= 17) { 
+    document.getElementById('answer').innerHTML =  'You can see rated R movies alone.'
+  } else if (userInput >= 13) {
+    document.getElementById('answer').innerHTML = 'You can see PG movies alone.'
+  } else {
+    document.getElementById('answer').innerHTML = 'You can see a G rated movie.'
+  }
+}
