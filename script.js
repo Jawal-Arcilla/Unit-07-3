59bb78e0759844230fab413aa3e681df2b6a419b
JawalArcilla <>
2021-05-06 19:33:12 +0000

    attempt #2

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ document.getElementById('button').addEventListener('click', age)
 let userInput = 0
 
 function age () {
-
   userInput = document.getElementById('input').value
   userInput = parseInt(userInput)
-  }
+}
   if (userInput >= 17) {
-    document.getElementById('answer').innerHTML = 'You can see a rated R movies alone.'
-  } else if (userInput >= 13) {
-    document.getElementById('answer').innerHTML = 'You can see PG movies alone.'
-  } else {
-    document.getElementById('answer').innerHTML = 'You can see a G rated movie.'
-  }
+  document.getElementById('answer').innerHTML = 'You can see a rated R movies alone.'
+} else if (userInput >= 13) {
+  document.getElementById('answer').innerHTML = 'You can see PG movies alone.'
+} else {
+  document.getElementById('answer').innerHTML = 'You can see a G rated movie.'
+}
